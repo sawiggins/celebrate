f0861f24f728668d9d72f9014c1bb0445fc3ea81
Stephanie Wiggins <stephawiggins@gmail.com>
2021-05-13 09:23:23 -0500

    feat: add filter button functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Button, Col, Container, Form, Row} from 'react-bootstrap'
 
 import CelebrationCard from './CelebrationCard.js'
 
+const CARDDATA = [
+  {
+    id: 1,
+    title: "Published a new version on an npm package.",
+    date: "05/04/2021",
+    category: "frontend",
+    description: "npm publish",
+    isDescriptionCode: true,
+  },
+  {
+    id: 2,
+    title: "Learned about recursion.",
+    date: "04/23/2021",
+    category: "other",
+    description: "Take a function/method and passs it to itself again.",
+    isDescriptionCode: false
+  },
+  {
+    id: 3,
+    title: "Add to database using Rails console .new and .create methods.",
+    date: "05/02/2021",
+    category: "backend",
+    description: "Model.new + Model.save(validation: true/false) Model.create (will run validations)",
+    isDescriptionCode: true,
+  },
+  {
+    id: 4,
+    title: "Lightbulb on bootstrap override placements.",
+    date: "05/05/2021",
+    category: "frontend",
+    description: '$variable-name: value; import bootstrap  h1 {value-to-change: value;}',
+    isDescriptionCode: true,
+  },
+  {
+    id: 5,
+    title: "Live coded for the first time at Code Connector’s Coding Dojo event.",
+    date: "04/03/2021",
+    category: "other",
+  }
+]
 const App = () => {
-return (
+
+  const [celebrationCards, setCelebrationCards] = useState(CARDDATA);
+  const [filteredCelebrationCards, setFilteredCelebrationCards] = useState(celebrationCards);
+
+  const filteredToRender = filteredCelebrationCards.map((card) => (
+      <CelebrationCard 
+        title={card.title}
+        date={card.date}
+        category={card.category}
+        description={card.description}
+        isDescriptionCode={card.isDescriptionCode}
+      />
+  ));
+
+  const frontend = celebrationCards.filter(card => card.category === 'frontend')
+  const backend = celebrationCards.filter(card => card.category === 'backend')
+  const other = celebrationCards.filter(card => card.category === 'other')
+
+
+  function filterFrontend() {
+    setFilteredCelebrationCards(frontend)
+  }
+
+  function filterBackend() {
+    setFilteredCelebrationCards(backend)
+  }
+
+  function filterOther() {
+    setFilteredCelebrationCards(other)
+  }
+
+  function removeFilter() {
+    setFilteredCelebrationCards(celebrationCards);
+  }
+  
+  return (
   <div className="App">
     <Container fluid>
     <Row className="hero flex-column text-center wavy-container py-5">
@@ -17,9 +92,10 @@ return (
           </Form.Group>
         </Form>
         <div className="filters">
-          <Button variant='frontend'>Frontend</Button>
-          <Button variant='backend'>Backend</Button>
-          <Button variant='other'>Other</Button>
+          <Button variant='frontend' onClick={() => filterFrontend()}>Frontend</Button>
+          <Button variant='backend' onClick={() => filterBackend()}>Backend</Button>
+          <Button variant='other' onClick={() => filterOther()}>Other</Button>
+          <Button onClick={() => removeFilter()}>All</Button>
         </div>
       </Col>
       <div className="wavy-bottom"></div>
@@ -27,7 +103,8 @@ return (
   </Container>
   <Container>
     <div className="card-columns">
-      <CelebrationCard 
+      {filteredToRender}
+      {/* <CelebrationCard 
         title="Published a new version on an npm package."
         date="05/04/2021"
         category="frontend"
@@ -64,7 +141,7 @@ return (
         title="Live coded for the first time at Code Connector’s Coding Dojo event."
         date="04/03/2021"
         category="other"
-      />
+      /> */}
     </div>
   </Container>
   </div>
